4a0190d44e8a29f6320f9d30c98432d394ca6974
meyers0 <169782812+meyers0@users.noreply.github.com>
2026-08-21 15:32:52 +0200

    Fix Node version matrix test

--- test.js
+++ test.js
@@ -26,11 +26,12 @@ assert('add -1+1', add(-1, 1), 0);
 assert('subtract 5-3', subtract(5, 3), 2);
 assert('subtract 0-5', subtract(0, 5), -5);
 
+const ver = parseInt(process.version.split('.')[0].substring(1));
+assert('Node version >= 20', ver >= 20 ? 'yes' : 'no', 'yes');
+
 console.log('\nResults: ' + passed + ' passed, ' + failed + ' failed');
 
 if (failed > 0) {
     process.exit(1);
 }
 
-const ver = parseInt(process.version.split('.')[0].substring(1));
-assert('Node version >= 20', ver >= 20 ? 'yes' : 'no', 'yes');
